b64e2c3a2e9e88309c1db25dac097b6ace261cd9
Henrique Teixeira Conti <49920796+henriqueconti@users.noreply.github.com>
2022-04-27 19:53:05 -0300

    add first commit

---
+++ src/main/java/br/com/checkpoint/microsservices/MicrosservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.checkpoint.microsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicesApplication.class, args);
+	}
+
+}
